da8c8631ccb99d58d74b47ec58bfc4a944eba707
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-03-08 10:16:55 +0530

    todo app complteted

---
+++ todo-app-react/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { TodoForm } from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+
+
+function  App () {
+  return (
+<div className='todo-app'>
+<TodoList />
+</div>
+  );
+}
+
+export default App;
